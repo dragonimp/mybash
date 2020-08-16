3263aef515223c42cbf743a786c58f003c51fc32
wengzs <dragonimp@impx.net>
2020-08-16 22:26:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 #just for install mybash.sh
 #put mybash.sh and this file at /root/bin for install
 
+chmod +x mybash.sh
+chmod +x googlecode.py
 cp mybash.sh /usr/local/bin
+cp googlecode.py /usr/local/bin
 ln -sf /usr/local/bin/mybash.sh /usr/bin/mybash
 #echo /usr/bin/mybash >> /etc/shells
 echo change user passwd login shell to /usr/bin/mybash
